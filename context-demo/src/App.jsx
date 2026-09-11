0fc6c72b3106920f6debec7484c60ed1163c66fd
Abdulqadir <aqbearing@gmail.com>
2026-09-11 20:15:39 +0530

    hook : useContext for passing the data only to the component that needs it to avoid prop drilling

---
+++ context-demo/src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./Header";
+import { UserContextProvider } from "./UserContextProvider";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>Dashboard</h1>
+      <Header />
+    </UserContextProvider>
+  );
+}
+
+export default App;
---
+++ context-demo/src/Avatar.jsx
@@ -0,0 +1,22 @@
+import { useContext, use } from "react"; // use is newer react 19 syntax, it is newer way to use the useContext and dont requires to follow the rules of hooks as it a object.
+import { UserContext } from "./UserContext";
+
+export const Avatar = () => {
+  const { user, setUser } = useContext(UserContext);
+  const { user: user2 } = use(UserContext);
+
+  const toggleTheme = () => {
+    setUser({
+      ...user,
+      theme: user.theme === "dark" ? "light" : "dark",
+    });
+  };
+
+  return (
+    <div>
+      <p>Welcome, {user.name}!</p>
+      <p>Current theme: {user2.theme}</p>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
+  );
+};
