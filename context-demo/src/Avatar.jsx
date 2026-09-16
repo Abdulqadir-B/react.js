a1fe5b07eb716241ad33a78b5800b0eabf4434a8
Abdulqadir <aqbearing@gmail.com>
2026-09-16 14:09:48 +0530

    added some comments for future ref

--- context-demo/src/Avatar.jsx
+++ context-demo/src/Avatar.jsx
@@ -1,9 +1,10 @@
-import { useContext, use } from "react"; // use is newer react 19 syntax, it is newer way to use the useContext and dont requires to follow the rules of hooks as it a object.
+// use is newer react 19 syntax, it is newer way to use the useContext and dont requires to follow the rules of hooks as it a object.
+import { useContext, use } from "react"; 
 import { UserContext } from "./UserContext";
 
 export const Avatar = () => {
   const { user, setUser } = useContext(UserContext);
-  const { user: user2 } = use(UserContext);
+  const { user: user2 } = use(UserContext); // example of user2 by using use hook
 
   const toggleTheme = () => {
     setUser({
